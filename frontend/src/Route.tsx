dafb3a296fc136c188a0758a1c2edc4f4d079c5e
Ayomi Perera <ayomi@swivelgroup.com.au>
2023-03-02 14:26:36 +0530

    first commit

--- frontend/src/Route.tsx
+++ frontend/src/Route.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import EmployeAction from './pages/EmployeAdd';
-import EmoData from './pages/EmpDataManager';
+import EmpData from './pages/EmpDataManager';
 import MainLayout from './theme/MainLayout';
 import PageNotfound from './pages/PageNotfound';
 
@@ -9,7 +9,7 @@ const RouteList = () => {
         <Routes>
             <Route element={<MainLayout/>}>
                 <Route path="/" element={<Navigate replace to="/employees/list" />}></Route>
-                <Route path="/employees/list" element={<EmoData/>}></Route>
+                <Route path="/employees/list" element={<EmpData/>}></Route>
                 <Route path="/employees/add" element={<EmployeAction/>}></Route>
                 <Route path="/employees/edit/:id" element={<EmployeAction/>}></Route>
                 <Route path='*' element={<PageNotfound/>}></Route>                
